9670e17412bf74764b3f3400622fd250dc9c034e
Shuo Sun <v_shuosun@tencent.com>
2020-01-26 22:33:27 -0500

    disable es replicas

--- modules/search.py
+++ modules/search.py
@@ -217,7 +217,7 @@ class Search():
         "settings" : {
             "index" : {
                 "number_of_shards" : 1,
-                "number_of_replicas" : 1
+                "number_of_replicas" : 0
                 }
             }
         }'''
