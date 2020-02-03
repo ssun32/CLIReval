bc7e766501a312aa4753d8d57ed6994cdf81d426
Shuo Sun <ssfei81@fb.com>
2020-02-02 21:36:24 -0500

    updated evaluate.sh in example folder

--- example/evaluate.sh
+++ example/evaluate.sh
@@ -1,10 +1,10 @@
 ref_file=example/en-de.ref.sgm
 mt_file=example/en-de.mt.sgm
 output_format=json
-analyzer=german
+target_langcode=de
 es_port=9200
 
 bash ./scripts/server.sh start
 es_port=$es_port bash ./scripts/server.sh check-start-wait
-python evaluate.py $ref_file $mt_file --output_format $output_format --analyzer $analyzer --port $es_port
+python evaluate.py $ref_file $mt_file --output_format $output_format --target_langcode target_langcode --port $es_port
 bash ./scripts/server.sh stop
